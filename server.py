73a1486c23cbfe0d65a6fef937052075045ed797
Vanessa Bell <vanessa.bell14@gmail.com>
2016-12-14 00:10:56 -0800

    run bottle MYAPP

--- server.py
+++ server.py
@@ -140,4 +140,4 @@ def logout():
 
 
 if __name__ == '__main__':
-    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    bottle.run(app=MYAPP, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
